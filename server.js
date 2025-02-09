650259b1098fcde1024fdb20a75c5505c51cb07b
Deepthi2001 <saideepthi2001@gmail.com>
2025-02-09 17:36:04 -0500

    Added server, Axios, and ChartJS

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use("/",express.static('public'));
+
+const budget = {
+    myBudget: [
+    {
+        title: "Rent",
+        budget: 350
+    },
+    {
+        title: "Groceries",
+        budget: 90
+    },
+    {
+        title: "Food",
+        budget: 30
+    },
+]};
+
+app.get("/hello",(req,res)=>{
+    res.send("HELLO WORLD! THIS IS SAIDEEPTHI");
+})
+
+app.get("/budget",(req,res)=>{
+    res.json(budget);
+})
+
+app.listen(port,()=>{
+    console.log(`This personal-budget app is listening at http://localhost:${port}`);
+})
